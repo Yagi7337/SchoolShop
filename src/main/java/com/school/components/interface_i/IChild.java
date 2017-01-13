fd54424754fbe40f092201f556bb9b7909fa2dac
Yagi7337 <yagi7337@gmail.com>
2017-01-13 11:21:25 +0200

    New v.#12

--- src/main/java/com/school/components/interface_i/IChild.java
+++ src/main/java/com/school/components/interface_i/IChild.java
@@ -2,7 +2,7 @@ package com.school.components.interface_i;
 
 import com.school.components.entity.Child;
 
-public interface ISchoolShop {
+public interface IChild {
 	public Child surchByID(int id);
 
 	public void createChild(Child child);
